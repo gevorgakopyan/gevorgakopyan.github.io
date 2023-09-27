77cb461f766eb5ed038fdfe52bbe974559013553
gevorgakopyan <gevorgakopyan01@gmail.com>
2023-09-27 14:35:39 -0700

    testing index.html

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
   return (
-    <div>
-       <h1 style={{ color: 'white' }}>AboutMe</h1>
+    <div className="about-me-container">
+      {/* <img src={myPicture} alt="Your Name" className="my-picture" /> */}
+      <div className="about-me-text">
+        <h1>Your Name</h1>
+        <p>
+          Hello! I'm Your Name. I am passionate about web development and have
+          experience in creating interactive and user-friendly web applications
+          using React. In my free time, I enjoy reading, hiking, and exploring
+          new technologies.
+        </p>
+      </div>
     </div>
   );
 };
